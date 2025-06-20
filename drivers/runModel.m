0f5cb8c691ed00e089c00532ed33f3a64b9921f1
bankows <kristofer.bank@gmail.com>
2025-06-20 15:47:50 -0400

    Running the extended version

--- drivers/runModel.m
+++ drivers/runModel.m
@@ -12,3 +12,6 @@ cd(fullfile(project_path, 'models'));
 
 %% just a simple stochastic simulation of gov. investment shock
 dynare('Model_FR_rate.mod', 'savemacro');
+
+%% just a simple stochastic simulation of gov. investment shock
+dynare('Model_HumanCapital_v0.mod', 'savemacro');
