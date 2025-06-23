edb19f4875218ccea476a19b610cc45ccd6c0fe4
bankows <kristofer.bank@gmail.com>
2025-06-23 09:32:29 -0400

    Some formatting cleaning

--- drivers/runPlots.m
+++ drivers/runPlots.m
@@ -80,60 +80,50 @@ vertModelComparison(results, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "effshock", "e
 
 %%
 function vertModelComparison(results, VarListToPlot, modelList, outputFileName)
-
     % Load objects and adjust settings
     utils.call.paths;
-
+    
     % Please specify the date range of the series
     dataRange = qq(1,1): qq(25,4);
-
+    
     % Plotting
     figure
-
+    
     % Create main tiledlayout
     t = tiledlayout(4, 2, 'TileSpacing', 'compact', 'Padding', 'compact');
-    
     h = gcf;
-
     set(h, 'Units', 'centimeters', 'Position', [0 0 17 15])
     set(h, 'DefaultTextInterpreter', 'latex');
     set(h, 'DefaultAxesTickLabelInterpreter', 'latex');
     set(h, 'DefaultLegendInterpreter', 'latex');
-
+    
     % Create nested tiledlayouts for each row
     for aVar = VarListToPlot
         nexttile;
         grid on
-        hold on 
-
-        title(replace(aVar, "_", "\_"), 'Interpreter', 'latex', 'FontWeight', 'bold', 'FontSize', 10);        
-
+        hold on
+        title(replace(aVar, "_", "\_"), 'Interpreter', 'latex', 'FontWeight', 'bold', 'FontSize', 10);
+        
         for aModel = modelList % for each panel
-
             % Plotting the data
-            plotData = results.(aModel).irfStruct.model.(aVar){dataRange};  % Accessing the specific variable for the current model
+            plotData = results.(aModel).irfStruct.model.(aVar){dataRange}; % Accessing the specific variable for the current model
             plot(plotData, "LineWidth", 2);
-
-        end 
-
+        end
+        
         hold off;
-
-            
+        
         % Setting of the x and y axis
         xtickformat(gca,'yQQQ');
-
-        set(gca ...
-            , 'Xtick', dater.toMatlab(dataRange(1:16:end)) ...
-            , 'Fontsize', 8 ...
-            , 'Box', 'off' ...
-            , 'TickLabelInterpreter', 'latex' ...
-            , 'XLimitMethod', 'tight' ...
+        set(gca, ...
+            'Xtick', dater.toMatlab(dataRange(1:16:end)), ...
+            'Fontsize', 8, ...
+            'Box', 'off', ...
+            'TickLabelInterpreter', 'latex', ...
+            'XLimitMethod', 'tight' ...
         );
-
     end
     
     fullFileName = fullfile(project_path, 'docs', [outputFileName '.png']);
     exportgraphics(t, fullFileName, 'BackgroundColor', 'none');
-    
     fprintf('Graph saved as: %s\n', fullFileName);
 end
