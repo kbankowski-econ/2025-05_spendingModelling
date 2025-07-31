d0679c9ae62c5aab6705c2030999fe684476b80f
bankows <kristofer.bank@gmail.com>
2025-07-31 22:00:14 +0200

    re-doing csv file and plotting

--- drivers/runPlots.m
+++ drivers/runPlots.m
@@ -60,7 +60,7 @@ end
 
 %% Plot comparison
 %vertModelComparison(resultsProc, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "Cgrd", "H", "Lab", "E", "effshock", "effgeshock"], ["Model_HumanCapital_epsi_ig" , "Model_HumanCapital_epsi_cge" , "Model_HumanCapital_epsi_cgrd", "Model_HumanCapital_epsieff30y", "Model_HumanCapital_epsieffcge30y"], 'epsiall_AE');
-vertModelComparison(resultsProc, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "Cgrd", "H", "Lab", "E", "effshock", "effgeshock"], ["EM_Model_HumanCapital_epsicgeLAGeff10y", "EM_Model_HumanCapital_epsicgeeff10y"], 'epsiall_EM');
+vertModelComparison(resultsProc, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "Cgrd", "H", "Lab", "E", "effshock", "effgeshock"], ["EM_Model_HumanCapital_epsiigeff10y", "EM_Model_HumanCapital_epsiigLAGeff10y", "EM_Model_HumanCapital_epsicgeeff10y", "EM_Model_HumanCapital_epsicgeLAGeff10y"], 'epsiall_EM');
 %vertModelComparison(resultsProc, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "Cgrd", "H", "Lab", "E", "effshock", "effgeshock"], ["Model_HumanCapital_epsi_ig", "EM_Model_HumanCapital_epsiig"], 'epsi_ig');;
 %vertModelComparison(resultsProc, ["yd", "C", "Ip", "Ig", "Cg", "Cge", "Cgrd", "H", "Lab", "E", "effshock", "effgeshock"], ["Model_HumanCapital_epsi_cge", "EM_Model_HumanCapital_epsicge"], 'epsi_cge');;
 
@@ -81,7 +81,7 @@ end
 
 end
 
-databank.toCSV(tempDatabank, "/Users/kk/Documents/0000-00_work/2025-05_spendingModelling/docs/csvFiles/figureNumbers.csv", qq(0, 4): qq(50, 4) , "Decimals", 3);
+databank.toCSV(tempDatabank, "/Users/kk/Documents/0000-00_work/2025-05_spendingModelling/docs/csvFiles/figureNumbers.csv", qq(0, 4): qq(50, 4) , "Decimals", 3, "Comments", false, "Class", false);
 
 %%
 function vertModelComparison(resultsProc, VarListToPlot, modelList, outputFileName)
